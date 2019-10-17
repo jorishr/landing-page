ac992480b2da40c8b7e5b51cd7ca92e8f94011b6
jorishr <joris@jorisr.com>
2019-10-17 23:24:06 +0200

    js for mobile menu added

---
+++ main.js
@@ -0,0 +1,14 @@
+const menu = document.getElementById('toggle-menu');
+const nav  = document.getElementById('nav-menu');
+const exit = document.getElementById('exit-menu');
+
+
+menu.addEventListener('click', (e) => {
+    nav.classList.toggle('hide-mobile');
+    e.preventDefault;
+})
+
+exit.addEventListener('click', (e) => {
+    nav.classList.add('hide-mobile');
+    e.preventDefault;
+})

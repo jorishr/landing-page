3aa0a34b7a7624e25dac2a07e7e43d4538110af1
jorishr <joris@jorisr.com>
2019-11-01 23:52:58 +0100

    add js script for auto-updating copyright year; reviewed sass and better comments

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const menu = document.getElementById('toggle-menu');
 const nav  = document.getElementById('nav-menu');
 const exit = document.getElementById('exit-menu');
 
-
 menu.addEventListener('click', (e) => {
     nav.classList.toggle('hide-mobile');
     e.preventDefault;
